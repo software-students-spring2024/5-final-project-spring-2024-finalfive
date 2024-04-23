8190da048b96a91b16be30a2e783698520e47017
RakSridhar23 <rss9243@nyu.edu>
2024-04-23 18:11:42 -0400

    added route

--- web_app/app.py
+++ web_app/app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('start.html')
+
+@app.route("/profile")
+def profile():
+    
+    return render_template("profile.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
