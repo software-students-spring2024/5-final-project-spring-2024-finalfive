a429936ea3a3051ffa44ec954d47067383143111
Jean Luis Adrover Torres <98844818+jladrover@users.noreply.github.com>
2024-04-24 06:14:58 -0400

    init query setup

---
+++ api.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent, Tool
+from langchain_openai import OpenAI
+import requests, os
+
+
+#premier : fc
+def search_statmuse(query, sport):
+    URL = f'https://www.statmuse.com/{sport}/ask/{query}'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    description_tag = soup.find('meta', attrs={'name': 'description'})
+    description_content = description_tag.get('content') if "Instant answers" not in description_tag.get('content') else "Sorry, I dont't understand your question."
+    return(description_content)
+
+statmuse_tool = Tool(
+    name = "Statmuse",
+    func = search_statmuse,
+    description = "A sports search engine. Use this more than normal search if the question is about NBA basketball, like 'who is the highest scoring player in the NBA?'. Always specify a year or timeframe with your search. Only ask about one player or team at a time, don't ask about multiple players at once."
+)
+
+
+llm = OpenAI(temperature=0, api_key=os.environ.get("openai_key"))
+tools = load_tools(["serpapi", "llm-math"], llm=llm, serpapi_api_key=os.environ.get("serpapi_key")) + [statmuse_tool]
+agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+
+# question = "Who has scored more points in the NBA this season, Jayson Tatum or Nikola Jokic? Can you subtract the difference in points between them?"
+
+# print(question)
+# print(agent.run(question))
--- app.py
+++ app.py
@@ -1,128 +1,90 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
-from werkzeug.security import generate_password_hash, check_password_hash
-import flask_login, mongomock
-import functools, os
+from hashlib import sha256
+import mongomock, requests
 from pymongo import MongoClient
+from api import *
 
 app = Flask(__name__)
-app.secret_key = 'super secret key'
-
-if os.getenv("test"):
-    app.config["MONGO_URI"] = mongomock.MongoClient()
-else:
-    URI = "mongodb://mongodb:27017/"
-    app.config["MONGO_URI"] = MongoClient(URI)
-
-connection = app.config["MONGO_CONN"]
 
 
-# Mock user profile data - will be deleted once MongoDB database is active
-user_profile = {
-    "email": "john.doe@example.com",
-    "password": "doeboy",
-}
+app.config["MONGO_URI"] = MongoClient("mongodb://localhost:27017")
+connection = app.config["MONGO_URI"]
+db = connection["stats"]
+users = db.users
 
-def login_required(func):
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        if "email" not in session:
-            return redirect(url_for("login", next=request.url))
-        return func(*args, **kwargs)
+try:
+    connection.admin.command("ping")  
+    print(" *", "Connected to MongoDB!")  
+except Exception as e:
+    print(" * MongoDB connection error:", e)
 
-    return secure_function
-# Initialize the login manager
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
+# @login_manager.user_loader
+# def user_loader(email):
+#     data = users.find_one({"email": email})
+#     print(data)
+#     #if data:
+#         #return data
+#     return render_template("login.html", error="User not found")
 
-class User(flask_login.UserMixin):
-    pass
 
-# Loads user from current session
-@login_manager.user_loader
-def user_loader(email):
-    #results = user_collection.find_one({'email' : email})
-    results = user_profile['email']
-    if not results:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-# Loads user from flask request
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get("email")
-    #results = user_collection.find_one({'email' : email})
-    results = user_profile['email']
-    if not results:
-        return
-
-    user = User()
-    user.id = email
-    return user
+@app.route("/")
+def display_login():
+    return render_template("login.html", error = False)
 
 # The route for the log-in page
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def login():
-    error = None
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        action = request.form['action']
-        
-        # Attempts to find a DB entry from the Mongo Database
-        results = user_profile['email']
+    username = request.form['username']
+    password = sha256(request.form["password"].encode()).hexdigest()
+    data = users.find_one({"username": username})
+    #print(f"Data: {data}")
+    if data is None:
+        return render_template("login.html", error = True)
+    if data["password"] == password:
+        return redirect(url_for("display_home", username = username))
+    return render_template("login.html", error = True)
 
-        # Checks to see if user is already is in the databse
-        if action == 'signin' and results and user_profile['password'] == "doeboy":
-            #check_password_hash(user_profile['password'], password):
-            user = User()
-            user.id = email
-            flask_login.login_user(user)
-            session["email"] = email
-            # Redirect to the home page upon successful login
-            return redirect(url_for("home"))
-        else:
-            error = "Invalid credentials"
-    return render_template("login.html", error=error)
+@app.route("/register")
+def display_register():
+    return render_template("register.html")
 
 #the route for registering
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
 def register():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-        existing_user = user_profile['email']
-        # Adds a Database entry if the user isn't already registered
-        if existing_user is None:
-            #hash_pass = generate_password_hash(password)
-            #user_collection.insert_one({'email' : email, 'password' : hash_pass, 'name' : name})
-            flash('Registration successful!', 'success')
-            return redirect(url_for('home')) 
-        return 'That email already exists!'
-    return render_template('register.html')
+    name = request.form['name']
+    username = request.form['username']
+    password = sha256(request.form["password"].encode()).hexdigest()
 
-# the route for the home page
-@app.route("/home")
-@login_required
-def home():
-    return render_template('start.html')
-
-@app.route("/profile")
-@login_required
-def profile():
-    return render_template("profile.html", profile=user_profile)
-
-@app.route("/deletion")
-def delete_profile():
-    # Implementation to delete  profile from db
-    #
-    #     
-    #
-
-    return render_template("profile.html")
+    if users.find_one({"username": username}):
+        return render_template("register.html", error = True)
+    users.insert_one({"name": name, "username": username, "password": password, "friends": []})
     
+    return render_template("login.html")
+
+@app.route("/home/<username>")
+def display_home(username):
+    return render_template("start.html", username = username)
+
+@app.route("/query/<sport>/<username>")
+def display_query(sport, username):
+    # if sport == "fc":
+    #     return render_template("query.html", sport = "Football")
+    return render_template("query.html", sport = sport, username = username)
+
+@app.route("/profile/<username>")
+def display_profile(username):
+    return render_template("profile.html", username = username)
+
+@app.route("/profile/<username>", methods=['POST'])
+def profile(username):
+    username = request.form["username"]
+    return render_template("profile.html", username=username)
+
+
+@app.route("/query/<sport>", methods=["POST"])
+def query(sport):
+    sport = request.form["sport"]
+    return render_template("query.html", sport = sport)
+
 if __name__ == "__main__":
     app.run(debug=True)
