598a4c266747a989cca1f6dac0d384ab2e7d0516
RakSridhar23 <rss9243@nyu.edu>
2024-04-29 19:07:12 -0400

    added a unittest for testing build

--- tests/test.py
+++ tests/test.py
@@ -1,2 +1,16 @@
+import unittest
+import pytest
+from app import app
 
-
+class Test(unittest.TestCase):
+    
+    def setUp(self):
+        
+        app.config['TESTING']= True
+        self.app = app.test_client()
+        app.config['PRESERVE_CONTEXT_ON_EXCEPTION']= False
+        
+    def test_login(self):
+        
+        response = self.app.post('/', data= dict(username="XYZ", password="admin"), follow_redirects=True)
+        self.assertIn(b'@XYZ', response.data)
