fffdf46ad26f40703054db4329b1091e9f931a37
denprud <denzelprud1@gmail.com>
2024-04-30 16:12:27 -0400

    added unit tests

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
-from flask import url_for
-from app import app  
+from app import app
+from stats_api import search_statmuse  
 
 @pytest.fixture
 def client():
@@ -13,7 +13,7 @@ def test_display_login(client):
     assert rv.status_code == 200
 
 def test_login(client):
-    rv = client.post('/', data={'username': 'test', 'password': 'test'})
+    rv = client.get('/', data={'username': 'test', 'password': 'test'})
     assert rv.status_code == 200
 
 def test_display_register(client):
@@ -28,18 +28,31 @@ def test_display_home(client):
     rv = client.get('/home/test')
     assert rv.status_code == 200
 
-def test_display_query(client):
-    rv = client.get('/query/sport/test')
-    assert rv.status_code == 200
-
 def test_display_profile(client):
     rv = client.get('/profile/test')
     assert rv.status_code == 200
 
-def test_profile(client):
-    rv = client.post('/profile/test', data={'username': 'test'})
+def test_display_addfriends(client):
+    rv = client.get('/addfriends/test')
     assert rv.status_code == 200
 
-def test_query(client):
-    rv = client.post('/query/sport', data={'sport': 'test'})
+def test_addfriends(client):
+    rv = client.post('/addfriends/test', data={'friend': 'test'})
     assert rv.status_code == 200
+
+def test_display_query(client):
+    rv = client.post('/query/test/', data={'sport': 'nba', 'query': 'brunson'})
+    assert rv.status_code == 200
+
+def test_search_statmuse():
+    # Test with a known query and sport
+    result = search_statmuse('who is the highest scoring player in the NBA', 'nba')
+    assert 'Sorry, I dont\'t understand your question.' not in result
+
+    # Test with an unknown sport
+    result = search_statmuse('who is the highest scoring player in the NBA', 'unknown_sport')
+    assert 'Sorry, I dont\'t understand your question.' in result
+
+    # Test with an unknown query
+    result = search_statmuse('unknown query', 'nba')
+    assert 'Sorry, I dont\'t understand your question.' in result
